e025694bde52152154873f2c7efa034e62f60ff3
Alexandre <alexandrefsoficial@gmail.com>
2022-10-11 20:27:24 -0300

    order object and related functions implemented

--- scripts.js
+++ scripts.js
@@ -1,18 +1,22 @@
+var order = new Object();
+order.dish=[]
+order.drink=[]
+order.dessert=[]
+order.price=0
 function add_dish(item_name,item_price){
-    var dish = [item_name,item_price];
-    alert(dish);
+    order.dish=[item_name,item_price];
+    console.log(order);
 }
 function add_drink(item_name,item_price){
-    var drink = [item_name,item_price];
-    alert(drink);
+    order.drink = [item_name,item_price];
+    console.log(order);
 }
 function add_dessert(item_name,item_price){
-    var dessert = [item_name,item_price];
-    alert(dessert);
+    order.dessert = [item_name,item_price];
+    console.log(order);
 }
 function add_order(){
-    let sum = dish[1] + drink[1] + dessert[1];
-    var order = [dish[0],drink[0],dessert[0],sum];
-    alert(order);
+    order.price= order.dish[1] + order.drink[1] + order.dessert[1];
+    console.log(order);
 }
 
